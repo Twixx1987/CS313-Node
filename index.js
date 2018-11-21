a028e049743ecdf8f78361743e1b983a98c05e0d
Shaun Densley <twixx1987@gmail.com>
2018-11-20 21:09:33 -0800

    updated filepath

--- index.js
+++ index.js
@@ -3,15 +3,6 @@ var parser = require('body-parser');
 const path = require('path');
 const rateCalc = require('./calculateRate');
 const PORT = process.env.PORT || 5000;
-const { Client } = require('pg');
-
-const connectionString =
-  'postgres://lhrvtbcbhnzcsy:0fc42a830f0df3a7b408e27daee55c72602c3a7cf1661569f88649f9f29d61dd@ec2-50-17-225-140.compute-1.amazonaws.com:5432/d3p017654khhj1';
-
-const db = new Client({
-    connectionString: connectionString,
-    ssl: true
-});
 
 express()
     .use(parser.urlencoded({ extended: false }))
@@ -26,17 +17,6 @@ express()
         weight: null,
         rate: null
     }))
-    .get('/getPerson', (req, res) => {
-        const { id } = req.query;
-
-        db.connect().then(() => {
-            db.query(`SELECT * FROM person WHERE id = ${id}`).then(result => {
-                res.setHeader('Content-Type', 'application/json');
-                res.send(JSON.stringify(result.rows[0]));
-                db.end();
-            });
-        });
-    })
     .post('/math', (req, res) => {
             let equation = req.body.lhs + req.body.operator + req.body.rhs;
 
@@ -64,25 +44,3 @@ express()
             });
     })
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-
-
-
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/getPerson', (req, res) => {
-      const { id } = req.query;
-
-      db.connect().then(() => {
-          db.query(`SELECT * FROM person WHERE id = ${id}`).then(result => {
-              res.setHeader('Content-Type', 'application/json');
-              res.send(JSON.stringify(result.rows[0]));
-              db.end();
-          });
-      });
-  })
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
